55b2b2d8dfac7202665530281dcc60c610226ce7
KitchenTable99 <ccbitt23@colby.edu>
2020-07-27 17:35:17 -0700

    minor tweaks to naming

--- mother_parsers.py
+++ mother_parsers.py
@@ -12,14 +12,14 @@ from tqdm import tqdm as progress
 class Mother():
 
     master_df = None
-    collection_year = 2014
+    collection_year = None
 
     def __init__(self, id_num):
         self.id_num = id_num
-        self.df_subset = Mother.master_df.loc[Mother.master_df['caseid'] == id_num]                 # the relevant rows of the df
-        self.dhs_clust = self.df_subset['clusterno'].iloc[0]
+        self.df_subset = Mother.master_df.loc[Mother.master_df['idhspid'] == id_num]                 # the relevant rows of the df
+        self.dhsid = self.df_subset['idhshid'].iloc[0]
         self.birth_year = self.df_subset['birthyear'].iloc[0]
-        self.collected_range = list(range(self.birth_year + 15, Mother.collection_year + 1))        # over what years was this mother surveyed
+        self.collected_range = list(range(self.birth_year + 14, Mother.collection_year + 1))        # over what years was this mother surveyed
         self.mother_age = [dyear - self.birth_year for dyear in self.collected_range]               # how old was she
         self.gave_birth = self.df_subset['kidbirthyr'].tolist()                                     # what years did she give birth
 
@@ -32,7 +32,7 @@ class Mother():
         # figure out how long
         final_length = len(self.collected_range)
         # create the constant lists
-        clust = list(itertools.repeat(self.dhs_clust, final_length))
+        clust = list(itertools.repeat(self.dhsid, final_length))
         id_num = list(itertools.repeat(self.id_num, final_length))
         # make a list of booleans for births
         kids = [True if year in self.gave_birth else False for year in self.collected_range]
@@ -47,7 +47,7 @@ class Mother():
     def __str__(self):
         output = ''
         output += f'id_num = {self.id_num}\n'
-        output += f'dhs_clust = {self.dhs_clust}\n'
+        output += f'dhsid = {self.dhsid}\n'
         output += f'birth year = {self.birth_year}\n'
         output += f'collected_range = {self.collected_range}\n'
         output += f'mother age = {self.mother_age}\n'
@@ -73,9 +73,11 @@ def main():
     cmd_args = commandLineParser()
     # assign Class variable to the correct DataFrame
     input_df = pd.read_csv(cmd_args.input_csv)
+    survey_year = input_df['year'].iloc[0]
+    Mother.collection_year = survey_year
     Mother.master_df = input_df
     # make a list of all unique mother id
-    id_num_list = input_df['caseid'].tolist()
+    id_num_list = input_df['idhspid'].tolist()
     id_num_list = list(dict.fromkeys(id_num_list))
     # create a Mother object for each id
     mother_list = [Mother(num) for num in progress(id_num_list, desc='Creating mother objects')]
@@ -83,7 +85,7 @@ def main():
     mother_arrays = [mother.genDataArray() for mother in mother_list]
     data = np.concatenate(mother_arrays)
     # make a new DataFrame and export as csv
-    df = pd.DataFrame(data=data, columns=['DHSCLUST', 'Mother ID#', 'Year', 'Mother\'s Age', 'Baby?'])
+    df = pd.DataFrame(data=data, columns=['IDHSHID', 'IDHSPID', 'Year', 'Mother\'s Age', 'Baby?'])
     df.to_csv(cmd_args.output_csv, index=False)
 
 if __name__ == '__main__':
