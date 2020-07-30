c9271a34bd15652e5e36384ec342926af8b9c0fc
KitchenTable99 <ccbitt23@colby.edu>
2020-07-30 13:38:23 -0700

    no longer need to pass shapefile on command line. recursively checks directories for any shapfile and prompts user for exact one.

--- create_rainfall_data.py
+++ create_rainfall_data.py
@@ -21,8 +21,7 @@ def commandLineParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--unit_code', required=True, type=int, help='the unit code that designates the area of interest. See ./resources/unit_name.txt for list of unit codes.')
     parser.add_argument('--distance', required=True, type=float, default=10., help='the maximum distance (in km) allowed between a DHS center and a precip grid center.')
-    parser.add_argument('--shapefile_path', required=True, type=str, help='the path to the .shp file in a shapefile folder. This folder should be expanded from a .zip file.')
-    parser.add_argument('--len_years', required=True, type=int, help='the number of years to use to fit each gamma distribution.')
+    parser.add_argument('--len_years', default=30, type=int, help='the number of years to use to fit each gamma distribution. Defaults to 30')
     parser.add_argument('--output_file', type=str, default='cleanGamma_data.csv', help='the name of the processed csv. Defaults to cleanGamma_data.csv')
     parser.add_argument('--windows', '-w', type=str, help='the file path for the list of the names of precip files.')
     parser.add_argument('--verbose', '-v', action='store_true', help='whether or not to see the intermediate progress bar')
@@ -32,15 +31,43 @@ def commandLineParser():
 
     return args
 
-def main():
-    # command-line arguments
-    cmd_args = commandLineParser()
-    # get rainfall sums
-    gdf = rainfall_sums.body(cmd_args)
-    # eye breathing room
+def getNum(biggest):
+    usr_inp = input('Shapefile number:  ')
+    if int(usr_inp) > biggest:
+        print('\nNot a valid number.')
+        getNum(biggest)
+
+    return int(usr_inp)
+
+def printClearLine():
     _, columns = os.popen('stty size', 'r').read().split()
     fancy_sep = ['-' for _ in range(int(columns))]
     print(''.join(fancy_sep)) 
+
+def main():
+    # command-line arguments
+    cmd_args = commandLineParser()
+    # find shapefiles recursively
+    shapefiles = []
+    shapefile_paths = []
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith('.shp'):
+                shapefiles.append(file)
+                shapefile_paths.append(os.path.join(root, file))
+    if len(shapefiles) == 0:
+        raise Exception('No shapefiles were found.')
+    else:
+        printClearLine()
+        print('Which shapefile would you like to use?')
+        for index, shapefile in enumerate(shapefiles):
+            print(f'[{index+1}]\t{shapefile}')
+    usr_shp_file = getNum(len(shapefiles))
+    cmd_args.shapefile_path = shapefile_paths[usr_shp_file - 1]
+    # get rainfall sums
+    gdf = rainfall_sums.body(cmd_args)
+    # eye breathing room
+    printClearLine()
     # get percentile data
     rainfall_list = gdf['Rainfall Totals'].tolist()
     percentiles = gamma_calculations.body(rainfall_list, cmd_args)
