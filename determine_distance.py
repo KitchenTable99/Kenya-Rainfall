0b97e3da72bf6d7ffaad9fa45e618135b4612870
KitchenTable99 <ccbitt23@colby.edu>
2020-08-06 11:38:40 -0700

    recursive search capabilities

--- determine_distance.py
+++ determine_distance.py
@@ -22,7 +22,6 @@ def commandLineParser():
         argparse.namespace: an argparse namespace representing the command line arguments
     '''
     parser = argparse.ArgumentParser()
-    parser.add_argument('shapefile_path', type=str, help='the path to the .shp file in a shapefile folder. This folder should be expanded from a .zip file.')
     parser.add_argument('num_stations', type=int, help='the minimum distance to the (nth) station will be returned. ')
     parser.add_argument('--graph', action='store_true', help='whether or not to graph the distance data')
     parser.add_argument('--distance_csv', action='store_true', help='whether or not to dump the distance data to a csv')
@@ -32,13 +31,78 @@ def commandLineParser():
 
     return args
 
+def getNum(biggest, name):
+    '''Gets a user input number
+    
+    Args:
+        biggest (int): largest number a user can enter
+        name (str): what the user is entering
+    
+    Returns:
+        int: the number the user entered
+    '''
+    usr_inp = input(f'{name.capitalize()} number:  ')
+    if int(usr_inp) > biggest:
+        getNum(biggest)
+
+    return int(usr_inp)
+
+def getFile(extention, name, cmd_args):
+    '''Finds files and returns user chosen path
+    
+    Args:
+        extention (str): the .extention of the desired file type (e.g. .pdf, .txt, .pickle)
+        name (str): what to call the file when prompting the user
+        cmd_args (argparse.namespace): command-line argumentss
+    
+    Returns:
+        str: the path of the chosen file
+    
+    Raises:
+        Exception: If there are no files of the desired type found, raises Exception
+    '''
+    # find shapefiles recursively
+    disp_name = []
+    paths = []
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith(extention):
+                disp_name.append(file)                                 # name for display
+                paths.append(os.path.join(root, file))                 # path for later use
+    if len(disp_name) == 0:
+        raise Exception(f'No {name}s were found.')
+    else:
+        printClearLine(cmd_args)
+        print(f'Which {name} would you like to use?')
+        for index, shapefile in enumerate(disp_name):
+            print(f'[{index+1}]\t{shapefile}')
+
+    usr_shp_file = getNum(len(disp_name), name)
+
+    return paths[usr_shp_file - 1]
+
+def printClearLine(cmd_args):
+    '''Prints a dashed line across the terminal window.
+    
+    Args:
+        cmd_args (argparse.namespace): contains the width of the terminal window
+    '''
+    fancy_sep = ['-' for _ in range(cmd_args.screen_width)]
+    print(''.join(fancy_sep))
+
 def main():
     # get command-line args
     cmd_args = commandLineParser()
     cmd_args.determine_distance = True
+    cmd_args.pickle = False
+    # add screen width
+    _, columns = os.popen('stty size', 'r').read().split()
+    cmd_args.screen_width = int(columns)
+    # get shapefile
+    cmd_args.shapefile_path = getFile('.shp', 'shapefile', cmd_args)
     # bring in station distances
     st_coords = fp.precipFileParser('./resources/precip_data/precip.1977', [4, 8], return_coords=True)
-    raw_distances_list = fp.shapeFileParser(cmd_args.shapefile_path, st_coords, cmd_args, testing=cmd_args.testing)
+    raw_distances_list = fp.shapeFileParser(cmd_args, st_coords, testing=cmd_args.testing)
     if cmd_args.no_pickle:
         pass
     else:
